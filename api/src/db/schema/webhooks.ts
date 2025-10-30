be0bb53849769e1435d74a2cfbf5b50a144ba01a
Moisés <moisesnetored@gmail.com>
2025-10-30 07:01:12 -0400

    feat: setup database

---
+++ api/src/db/schema/webhooks.ts
@@ -0,0 +1,19 @@
+import { integer } from "drizzle-orm/pg-core";
+import { timestamp } from "drizzle-orm/pg-core";
+import { jsonb } from "drizzle-orm/pg-core";
+import { pgTable, text } from "drizzle-orm/pg-core";
+import {uuidv7} from 'uuidv7'
+
+export const webhooks = pgTable('webhooks', {
+  id: text().primaryKey().$defaultFn(() => uuidv7()),
+  method: text().notNull(),
+  pathname: text().notNull(),
+  ip: text().notNull(),
+  statusCode: integer().notNull().default(200),
+  contentType: text(),
+  contentLength: integer(),
+  queryParams: jsonb().$type<Record<string, string>>(),
+  headers: jsonb().$type<Record<string, string>>().notNull(),
+  body: text(),
+  createdAt: timestamp().notNull().defaultNow(),
+})
